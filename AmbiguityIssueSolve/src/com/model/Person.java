ebef12a43e86cd78c43836cd8cc6e5ec37ced01d
Shubhangi-Shelke <shubhangishelke9272@gmail.com>
2026-04-22 18:01:43 +0530

    Constructor Ambiguity Issue Test

---
+++ AmbiguityIssueSolve/src/com/model/Person.java
@@ -0,0 +1,17 @@
+package com.model;
+
+public class Person {
+	
+	public Person(String n2, int i2) {
+		System.out.println("constructor Executed");
+	}
+
+
+	public Person(String name, double id) {
+		System.out.println("Double Executed");
+	}
+	
+	public Person(String city, String country) {
+		System.out.println("String Executed");
+	}
+}
