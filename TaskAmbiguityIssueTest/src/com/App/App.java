1557171a34c723e04c61416b0b6e4d480f6016e0
Shubhangi-Shelke <shubhangishelke9272@gmail.com>
2026-04-23 22:59:08 +0530

    Constructor Ambiguity Issue Test

---
+++ TaskAmbiguityIssueTest/src/com/App/App.java
@@ -0,0 +1,13 @@
+package com.App;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
+	}
+
+}
---
+++ TaskAmbiguityIssueTest/src/com/model/Employee.java
@@ -0,0 +1,23 @@
+package com.model;
+
+public class Employee {
+
+	
+	public Employee(String name,int id,String city  )
+	{
+		System.out.println("Constructor Executed");
+	}
+	
+	
+	public Employee(String name,String id,String city  )
+	{
+		System.out.println("  String Constructor Executed");
+	}
+	
+	
+	
+	public Employee(double name,double id,double city  )
+	{
+		System.out.println(" double  Constructor Executed");
+	}
+}
